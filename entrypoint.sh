29d77ef00f2035d7aa8cf01c7d1a42de4f69e993
johnmauser333 <86488300+johnmauser333@users.noreply.github.com>
2022-04-25 17:31:03 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc/xray"
+DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
-DIR_TMP1="$(mktemp -d)"
 
 ID=69414c6d-2516-41c9-92de-3fcee09e3ad1
 AID=0
@@ -37,21 +36,16 @@ cat << EOF > ${DIR_TMP}/heroku.json
 EOF
 
 # Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/XTLS/Xray-core/releases/download/v1.5.4/Xray-linux-64.zip -o ${DIR_TMP}/xray.zip
-busybox unzip ${DIR_TMP}/xray.zip -d ${DIR_TMP}
-
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP1}/v2ray_dist.zip
-busybox unzip ${DIR_TMP1}/v2ray_dist.zip -d ${DIR_TMP1}
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
-${DIR_TMP1}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
 # Install V2Ray
-install -m 755 ${DIR_TMP1}/v2ray ${DIR_RUNTIME}
-install -m 755 ${DIR_TMP}/xray ${DIR_RUNTIME}
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
-rm -rf ${DIR_TMP1}
 
 # Run V2Ray
-${DIR_RUNTIME}/xray -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
